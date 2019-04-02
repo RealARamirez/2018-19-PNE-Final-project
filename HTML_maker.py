e7b9947428fb9c3cb44395b843be9d390ca882d8
RealARamirez <Antoniomanuelramirezmarquez@gmail.com>
2019-04-02 20:44:00 +0200

    Python program for designing the different HTML files.

---
+++ HTML_maker.py
@@ -0,0 +1,17 @@
+# Python program for designing the html files that would be used in this project
+# Define how all html files will start
+opening = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Genome application</title>
+</head>
+<body background="background_image.jpg" marginwidth="0" marginheight="0" >
+"""
+
+# Define how all html files will end
+closing = """
+</body>
+</html>
+"""
