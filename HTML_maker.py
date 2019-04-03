8be2f8d8516ed24ef43058511727e8b1030bfa59
realantm <am.ramirezm.2018@alumnos.urjc.es>
2019-04-03 18:22:03 +0200

    Create the HTML objects that are needed for the application.

--- HTML_maker.py
+++ HTML_maker.py
@@ -4,18 +4,41 @@ opening = """
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Genome application</title>
+    <meta charset="UTF-8">"""
+
+
+# HTML part that defines the title
+def title(title):
+    return """
+        <title>{}</title>""".format(title)
+
+
+# Part of the HTML that ends the head and begin the body
+medium = """
 </head>
 <body background="background_image.jpg" style=" no-repeat center center fixed; 
 -webkit-background-size: cover; 
 -moz-background-size: cover; 
 -o-background-size: cover; 
-background-size: cover">
-"""
+background-size: cover">"""
 
 # Define how all html files will end
 closing = """
 </body>
-</html>
-"""
+</html>"""
+
+class HTMLFile:
+    def __init__(self):
+        return
+
+    def error(self):
+        file = opening
+        file += title("Error")
+        file += medium
+        file += """
+            <h1>Error page</h1>
+            <p>This page is not recognize, if you want to go back to the initial page:</p>
+            <a href="/">Click here.</a>
+        """
+        file += closing
+        return file
