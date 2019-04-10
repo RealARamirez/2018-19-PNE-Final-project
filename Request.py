117aa4c0040442f194950c6c63cd74497b37fc06
realantm <am.ramirezm.2018@alumnos.urjc.es>
2019-04-10 18:42:11 +0200

    Object that according to the request develop different functions.

--- Request.py
+++ Request.py
@@ -1,3 +1,4 @@
+from Client import Client
 # Program for creating an object that allow us to threat the request as an object
 def mapper(dictionary, arg):
     value = [False, 0]
@@ -29,10 +30,15 @@ class Request:
             for elem in self.request.split("?")[1].split("&"):
                 if elem == "json=1": self.data["json"] = "yes"
                 if "=" in elem:
-                    self.data["parameter"+str(b)] = {}
+                    self.data["parameter" + str(b)] = {}
                     self.data["parameter" + str(b)]["type"] = elem.split("=")[0]
                     self.data["parameter" + str(b)]["value"] = elem.split("=")[1]
                     b += 1
+        # Will reassign the parameter gene to its ID
+        for key in self.data:
+            if key.startswith("parameter"):
+                if self.data[key]["type"] == "gene":
+                    self.data[key]["value"] = Client("/xrefs/symbol/homo_sapiens/{}?content-type=application/json".format(self.data[key]["value"]))[0]["id"]
         return
 
     # This method will assign the str value of the request to the object
@@ -41,8 +47,10 @@ class Request:
 
     # This method will tell us if the user is asking for a Json object or not
     def isjson(self):
-        if self.data["json"] == "yes": return True
-        else: return False
+        if self.data["json"] == "yes":
+            return True
+        else:
+            return False
 
     # The method will give the client the endpoint it must use according to the request
     # It will give a dictionary
@@ -50,29 +58,35 @@ class Request:
     # The key value will give that error message or that path respectively
     def answer(self):
         # A set that achieve the valid possible endpoints
-        endpoints = {"/listSpecies", "/karyotype", "/chromosomeLength", "/geneSeq", "/geneInfo", "/geneCal", "/geneList"}
+        endpoints = {"/listSpecies", "/karyotype", "/chromosomeLength", "/geneSeq", "/geneInfo", "/geneCalc", "/geneList"}
         if self.data["endpoint"] in endpoints:
-            if self.data["endpoint"] == "/listSpecies": return {"type": "client", "value": "/info/species?content-type=application/json"}
+            if self.data["endpoint"] == "/listSpecies":
+                return {"type": "client", "value": "/info/species?content-type=application/json"}
             elif self.data["endpoint"] == "/karyotype":
                 if mapper(self.data, "specie")[0]:
-                    return {"type": "client", "value":"/info/assembly/{}?content-type=application/json".format(self.data[mapper(self.data, "specie")[1]]["value"])}
-                else: return {"type": "error", "value": "The function kayotype requires an specie parameter."}
+                    return {"type": "client", "value": "/info/assembly/{}?content-type=application/json".format(self.data[mapper(self.data, "specie")[1]]["value"])}
+                else:
+                    return {"type": "error", "value": "The function kayotype requires an specie parameter."}
             elif self.data["endpoint"] == "/chromosomeLength":
                 if mapper(self.data, "specie")[0] and mapper(self.data, "chromo")[0]:
                     return {"type": "client", "value": "/info/assembly/{}?content-type=application/json".format(self.data[mapper(self.data, "specie")[1]]["value"])}
-                else: return {"type": "error", "value": "The function chromosome Length requires an specie and chromo parameter."}
-            elif self.data["endpoint"] == "/geneSeq" or self.data["endpoint"] == "/geneCal":
+                else:
+                    return {"type": "error", "value": "The function chromosome Length requires an specie and chromo parameter."}
+            elif self.data["endpoint"] == "/geneSeq" or self.data["endpoint"] == "/geneCalc":
                 if mapper(self.data, "gene")[0]:
-                    return {"type": "client", "value": "/sequence/id/{}?content-type=text/plain".format(self.data[mapper(self.data, "specie")[1]]["gene"])}
-                else: return {"type": "client", "value": "This function requires a gene parameter."}
+                    return {"type": "client", "value": "/sequence/id/{}?content-type=text/plain".format(self.data[mapper(self.data, "gene")[1]]["value"])}
+                else:
+                    return {"type": "client", "value": "This function requires a gene parameter."}
             elif self.data["endpoint"] == "/geneInfo":
                 if mapper(self.data, "gene")[0]:
-                    return {"type": "client", "value": "/lookup/id/{}?content-type=application/json".format(self.data[mapper(self.data, "specie")[1]]["gene"])}
-                else: return {"type": "error", "value": "The function gene Info requires a gene parameter."}
+                    return {"type": "client", "value": "/sequence/id/{}?content-type=text/plain".format(self.data[mapper(self.data, "gene")[1]]["value"])}
+                else:
+                    return {"type": "error", "value": "The function gene Info requires a gene parameter."}
             elif self.data["endpoint"] == "/geneList":
-                if mapper(self.data, "gene")[0]:
-                    return {"type": "client"}
-                else: return {"type": "error", "value": "The function gene List requires a gene parameter."}
+                if mapper(self.data, "chromo")[0] and mapper(self.data, "stat") and mapper(self.data, "end"):
+                    return {"type": "client", "value": "/overlap/region/human/{}:{}-{}?feature=gene".format(self.data[mapper(self.data, "chromo")[1]]["value"], self.data[mapper(self.data, "stat")[1]]["value"], self.data[mapper(self.data, "end")[1]]["value"])}
+                else:
+                    return {"type": "error", "value": "The function gene List requires a gene parameter."}
         else:
             return {"type": "error", "value": "Sorry, we do not perform {} operation, please check that you have typed it correctly.".format(self.data["endpoint"][1:])}
 
@@ -92,7 +106,7 @@ class Request:
         return List
 
 
-R = Request("/chromosomeLength?specie=mouse&chromo=18")
+R = Request("/geneCalc?gene=FRAT1")
 print(R.data)
 print(R.isjson())
 print(R.answer())
