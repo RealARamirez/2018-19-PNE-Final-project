84a967f33c1581d83f9c384788c3554f0982c123
realantm <am.ramirezm.2018@alumnos.urjc.es>
2019-04-10 19:49:34 +0200

    Object for developing the different HTML files that are needed in the web server.

--- HTML_maker.py
+++ HTML_maker.py
@@ -32,6 +32,7 @@ closing = """
 """.rstrip("\n")
 
 class HTMLFile:
+    # This method will create an error page with a given message
     def error(self, message):
         file = opening
         file += title("Error")
@@ -44,6 +45,7 @@ class HTMLFile:
         file += closing
         return file
 
+    # This method will create the HTML page for list Species
     def listSpecies(self, data, parameters):
         file = opening
         file += title("list Species")
@@ -71,3 +73,49 @@ class HTMLFile:
             file += "Specie number {} is {} most common known as {}.\n".format((b + 1), data[b]["name"],data[b]["common_name"])
         file += closing
         return file
+
+    # This method will create an HTML answer for the karyotype
+    def karyotype(self, data):
+        file = opening
+        file += title("karyotype")
+        file += medium
+        file += """
+    <h1>Karyotype page</h1>
+                """.rstrip("\n")
+        Message = "The karyotype is compassed by the following elements: "
+        for elem in data["karyotype"]:
+            if elem == "MT": continue
+            else: Message += elem
+        file += """
+    <p>{}.</p>
+        """.format(Message)
+        file += closing
+        return file
+
+    # This method will create the HTML page for the answer to the chromosome length
+    def chromosomeLength(self, data, parameters):
+        file = opening
+        file += title("chromosomeLength")
+        file += medium
+        file += """
+    <h1>Chromosome Length page</h1>
+        """.rstrip("\n")
+        value = [False]
+        name = "none"
+        for elem in parameters:
+            if elem["type"] == "chromo":
+                name = elem["value"]
+        for elem in data["top_level_region"]:
+            if elem["name"] == name:
+                value = [True, elem["length"]]
+        if value[0]:
+            file += """
+    <p>Your chromosome has a length of {}.</p>
+            """.format(value[1]).rstrip("\n")
+        else:
+            file += """
+    <p>Your chromosome has not been found in our data base.</p>
+            """.rstrip("\n")
+        file += closing
+        return file
+
