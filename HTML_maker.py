8f20306f3e6345fcae175f5dd68d1aecfe3a63bd
realantm <am.ramirezm.2018@alumnos.urjc.es>
2019-04-10 19:16:22 +0200

    Object for developing the different HTML files that are needed in the web server.

--- HTML_maker.py
+++ HTML_maker.py
@@ -38,8 +38,36 @@ class HTMLFile:
         file += medium
         file += """
     <h1>Error page</h1>
-    <p>This page is not recognize, if you want to go back to the initial page:</p>
+    <p>{}, if you want to go back to the initial page:</p>
     <a href="/">Click here.</a>
+        """.format(message).rstrip("\n")
+        file += closing
+        return file
+
+    def listSpecies(self, data, parameters):
+        file = opening
+        file += title("list Species")
+        file += medium
+        file += """
+    <h1>List Species page</h1>
         """.rstrip("\n")
+        top = len(data["species"])
+        for elem in parameters:
+            if elem["type"] == "limit":
+                if elem["value"].isdigit():
+                    if int(elem["value"]) <= len(data["species"]):
+                        top = int(elem["value"])
+                    else:
+                        top = len(data["species"])
+                        file += """
+    <p>Your limit is higher than the number of species, so all the species will be listed</p>
+                        """.rstrip("\n")
+                else:
+                    top = len(data["species"])
+                    file += """
+    <p>Your limit is not a number, all species will be listed.</p>
+                    """.rstrip("\n")
+        for b in range(top):
+            file += "Specie number {} is {} most common known as {}.\n".format((b + 1), data[b]["name"],data[b]["common_name"])
         file += closing
         return file
