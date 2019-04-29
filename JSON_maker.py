52687520dbec42f9b5a2aa12a971df297c6ae9d2
RealARamirez <Antoniomanuelramirezmarquez@gmail.com>
2019-04-29 18:35:38 +0200

    Final project finished.

--- JSON_maker.py
+++ JSON_maker.py
@@ -114,6 +114,4 @@ def chooser(endpoint, message, data, parameters):
 
 # Function that will create the json file
 def JSONFile(endpoint, message, data, parameters):
-    Dictionary = chooser(endpoint, message, data, parameters)
-    Dictionary["main_page"] = "http://localhost:8000/"
-    return json.dumps(Dictionary)
+    return json.dumps(chooser(endpoint, message, data, parameters))
